54b0dc4a9c0b3661d46598b81e91f0614b807361
rabebe <ruthie.abebe@gmail.com>
2026-01-06 15:08:15 -0500

    Test: Add Jest configuration and setup files

---
+++ jest.config.mjs
@@ -0,0 +1,14 @@
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files
+  dir: './',
+})
+
+const config = {
+  // Add more setup options before each test is run
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+}
+
+export default createJestConfig(config)
